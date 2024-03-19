5f4d4f5392c164de221933f5ae39773cc770ea47
12118954 <leonardodavinte13@gmail.com>
2024-03-19 19:00:41 -0300

    edit motor to 3

--- ExPoo/src/Caminhão.java
+++ ExPoo/src/Caminhão.java
@@ -1,7 +1,7 @@
 import java.lang.String;
 public class Caminhão {
     private int id;
-    private String motor300;
+    private String motor3;
     private String fabricante;
     private String modelo;
     private double anofabricacao;
