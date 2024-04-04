5afd5dad7fc58ed29c3f1a54f8a095ecbaa04e44
Nicolas Kevin <143231987+NicolasKevin2@users.noreply.github.com>
2024-04-04 20:25:06 -0300

    Add files via upload

---
+++ Exercicio_aula/src/exercicio.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class exercicio {
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JButton button1;
+public exercicio() {
+    button1.addActionListener(new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            salvar();});
+            painel.addjButtonSalvar;
+        }
+    });
+}
+}
